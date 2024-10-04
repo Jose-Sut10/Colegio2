895d0ed0fa26af7cfcd940ff81c49abd10d548d8
José Sut <jsut914@gmail.com>
2024-10-04 09:51:45 -0600

    Diseñando el apartado de misión y visión

--- src/components/home/main/main.jsx
+++ src/components/home/main/main.jsx
@@ -1,11 +1,14 @@
 import Carreras from "./carreras/carreras";
 import Carrusel from "./carrusel/carrusel";
+import './main.css'
+import MisionVision from "./misionVision/info";
 
 const Main = ({carreras})=>{
     return <main className="contenido">
         <Carrusel/>
         <section className="container">
             <Carreras carreras={carreras}/>
+            <MisionVision/>
         </section>
     </main>
 }
---
+++ src/components/home/main/misionVision/info.jsx
@@ -0,0 +1,28 @@
+import './info.css';
+
+const MisionVision = () =>{
+    return <div className="misionVision">
+        <div className="misVis">
+            <h3>Misión</h3>
+            <p>Establecer condiciones propicias para recrear un modelo universitario de identidad jesuita,
+                inspirado en la espiritualidad y pedagogía ignaciana, que promueve la educación, la investigación
+                y la proyección como medio para incidir significativamente en el mejoramiento de las
+                vidas de las personas, incrementar el bienestar de la sociedad y cuidar la calidad del
+                entorno donde se desarrollan, contribuyendo, de esa manera concreta, a construir y sostener
+                el bien general.</p>
+            <a href="https://youtube.com">Más información</a>
+        </div>
+        <div className="misVis">
+                <h3>Visión</h3>
+                <p>Establecer condiciones propicias para recrear un modelo universitario de identidad jesuita,
+                inspirado en la espiritualidad y pedagogía ignaciana, que promueve la educación, la investigación
+                y la proyección como medio para incidir significativamente en el mejoramiento de las
+                vidas de las personas, incrementar el bienestar de la sociedad y cuidar la calidad del
+                entorno donde se desarrollan, contribuyendo, de esa manera concreta, a construir y sostener
+                el bien general.</p>
+            <a href="https://youtube.com">Más información</a>
+        </div>
+    </div>
+}
+
+export default MisionVision;
