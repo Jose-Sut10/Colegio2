1dd0adfb7405be5f0656bdb666a0353d5ccff562
José Sut <jsut914@gmail.com>
2024-10-05 23:15:01 -0600

    Modificando el carrusel

--- src/components/home/main/carrusel/carrusel.jsx
+++ src/components/home/main/carrusel/carrusel.jsx
@@ -1,10 +1,37 @@
-import "./carrusel.css";
+import { useState, useEffect } from 'react';
+import './carrusel.css';
+const Carrusel = () => {
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const images = [
+    '/img/clase.jpg',
+    '/img/aulas.jpg',
+    '/img/chava.jpg',
+    '/img/campus1.jpg',
+    '/img/campus2.jpg',
+    '/img/campus3.jpg'
+];
 
-const Carrusel = () =>{
-    return(
-        <div className="containerCarrusel">
-        </div>
-    );
+useEffect(() => {
+    const intervalId = setInterval(() => {setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);}, 6000);
+
+    return () => clearInterval(intervalId);}, [images.length]);
+
+    const goToPrevious = () => {
+        setCurrentIndex((prevIndex) => (prevIndex - 1 + images.length) % images.length);
+    };
+
+    const goToNext = () => {
+        setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+    };
+
+    return (
+        <div className="containerCarrusel" style={{
+        backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${images[currentIndex]})`
+    }}>
+        <i class="fa-solid fa-angles-left left" onClick={goToPrevious}></i>
+        <i class="fa-solid fa-angles-right next" onClick={goToNext}></i>
+    </div>
+);
 }
 
 export default Carrusel;
